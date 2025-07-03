1a4a8387489780b4f82aefcb803aabd9e18da41c
geronimofretes <gfretes@itba.edu.ar>
2025-07-02 22:24:44 -0300

    add vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Inter } from 'next/font/google';
@@ -36,6 +37,7 @@ export default function RootLayout({
         className="font-sans"
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
